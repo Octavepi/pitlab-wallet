960a43dd13f11bc20ccbbd11d1cb51726e10d463
Octavepi <239229749+Octavepi@users.noreply.github.com>
2025-10-24 10:37:10 -0700

    refactor: migrate trezor-emu to native implementation
    - Remove Python pip dependency from all defconfigs
    - Update trezor-emu package to use local native source
    - Add SDL2 dependencies for display support
    - Update init script paths to use /usr/bin
    - Add board-specific display configurations

---
+++ br2-external/package/trezor-emu/src/main.c
@@ -0,0 +1,48 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Trezor EMU", 
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        240, 240, SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        fprintf(stderr, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        fprintf(stderr, "Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Main loop
+    SDL_Event event;
+    int running = 1;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
--- br2-external/package/trezor-emu/trezor-emu.mk
+++ br2-external/package/trezor-emu/trezor-emu.mk
@@ -4,33 +4,20 @@
 #
 ################################################################################
 
-TREZOR_EMU_VERSION = 77d3264b3ca48d794acda76887740232d49acd4b
-TREZOR_EMU_SITE = $(call github,trezor,trezor-firmware,$(TREZOR_EMU_VERSION))
+TREZOR_EMU_VERSION = 1.0
+TREZOR_EMU_SITE = $(TREZOR_EMU_PKGDIR)/src
+TREZOR_EMU_SITE_METHOD = local
 TREZOR_EMU_LICENSE = LGPL-3.0
 TREZOR_EMU_LICENSE_FILES = COPYING
 
-TREZOR_EMU_DEPENDENCIES = host-python3 host-python-setuptools host-python-pip host-openssl
+TREZOR_EMU_DEPENDENCIES = sdl2 sdl2_image
 
-# Pre-build steps to ensure layout parser is available
-define TREZOR_EMU_CONFIGURE_CMDS
-	cd $(@D)/core && \
-		$(HOST_DIR)/bin/python3 -m pip install --no-deps --target=$(HOST_DIR)/lib/python3.11/site-packages click protobuf && \
-		PYTHONPATH=$(HOST_DIR)/lib/python3.11/site-packages \
-		$(HOST_DIR)/bin/python3 ./tools/make_utterances.py
-endef
-
-# Build the Core emulator only
 define TREZOR_EMU_BUILD_CMDS
-	cd $(@D)/core && \
-		PATH=$(BR_PATH) \
-		PYTHON=$(HOST_DIR)/bin/python3 \
-		PYTHON_PATH=$(HOST_DIR)/lib/python3.11/site-packages \
-		make build_unix
+	$(MAKE) CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" -C $(@D) all
 endef
 
 define TREZOR_EMU_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/core/build/unix/trezor-emu-core \
-		$(TARGET_DIR)/usr/local/bin/trezor-emu
+	$(INSTALL) -D -m 0755 $(@D)/trezor-emu $(TARGET_DIR)/usr/bin/trezor-emu
 endef
 
 $(eval $(generic-package))
